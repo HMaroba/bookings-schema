7ef29ba5c36bfacef348f91a4d128c864c9dbcf0
Coffee58511867 <marobahlalele910@gmail.com>
2024-07-20 17:41:12 +0200

    Created api

--- src/app/doctors/page.tsx
+++ src/app/doctors/page.tsx
@@ -1,7 +1,27 @@
-export default function DoctorsPage() {
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+export default async function DoctorsPage() {
+  const doctors = await prisma.doctor.findMany({
+    include: {
+      monday: true,
+      friday: true,
+    },
+  });
+  console.log(doctors);
+
   return (
     <div>
       <p>Doctors List</p>
+      <p>
+        {
+            doctors.map((user: any) => (
+                <div key={user.id} className="p-10">
+                  <p>{user.name}</p>
+                </div>
+            ))
+        }
+      </p>
     </div>
   );
 }
