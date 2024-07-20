dd816bc7cc968385a1bf61b7c912b6906d87fb35
Coffee58511867 <marobahlalele910@gmail.com>
2024-07-20 17:32:43 +0200

    Created api

--- src/app/api/v1/register/route.ts
+++ src/app/api/v1/register/route.ts
@@ -22,7 +22,28 @@ export async function POST(request: NextRequest) {
   } catch (error) {
     return NextResponse.json({
       status: 500,
-      message: "Server error" +error,
+      message: "Server error" + error,
+    });
+  }
+}
+
+export async function GET() {
+  try {
+    const doctors = await prisma.doctor.findMany({
+      include: {
+        monday: true,
+        friday: true,
+      },
+    });
+
+    return NextResponse.json({
+      status: 200,
+      doctorList: doctors,
+    });
+  } catch (error) {
+    return NextResponse.json({
+      status: 500,
+      message: "Server error",
     });
   }
 }
